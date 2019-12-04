bfe875912d1f705685d48e72b33bfbcee757a6f9
Spencer Jensen <swj96@byu.edu>
2019-12-03 22:29:05 -0700

    Login page

--- Jaws-Intex/Controllers/HomeController.cs
+++ Jaws-Intex/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Jaws_Intex.Controllers
 {
@@ -12,6 +13,30 @@ namespace Jaws_Intex.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection form, bool rememberMe = false)
+        {
+            String email = form["Email address"].ToString();
+            String password = form["Password"].ToString();
+
+            if (string.Equals(email, "greg@test.com") && (string.Equals(password, "greg")))
+            {
+                FormsAuthentication.SetAuthCookie(email, rememberMe);
+
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
