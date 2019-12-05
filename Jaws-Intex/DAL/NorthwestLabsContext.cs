82707283ee6194fcaba24decef391c5e699c9198
jacoby003 <jacoby003@outlook.com>
2019-12-05 09:57:20 -0700

    Added table and model for Knowledge Base

--- Jaws-Intex/DAL/NorthwestLabsContext.cs
+++ Jaws-Intex/DAL/NorthwestLabsContext.cs
@@ -19,5 +19,6 @@ namespace Jaws_Intex.DAL
         public DbSet<Test> Tests { get; set; }
         public DbSet<CompoundStatus> CompoundStatuses { get; set; }
         public DbSet<Status> Statuses { get; set; }
+        public DbSet<KnowledgeBase> knowledgeBases { get; set; }
     }
 }
---
+++ Jaws-Intex/Models/KnowledgeBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Jaws_Intex.Models
+{
+    [Table("Knowledge_Base")]
+    public class KnowledgeBase
+    {
+        [Key]
+        public int KnowledgeBaseId { get; set; }
+
+        [Display(Name = "Title")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Author")]
+        [Required]
+        public string Author { get; set; }
+        
+        [Display(Name = "Date")]
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Contents")]
+        [Required]
+        public string Contents { get; set; }
+    }
+}
